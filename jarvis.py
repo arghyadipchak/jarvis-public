944875e9e0b37f2875a3349f62ee12b18023d8f9
Arghyadip Chakraborty <sherlock@localhost.localdomain>
2020-06-14 20:59:24 +0530

    Update

--- jarvis.py
+++ jarvis.py
@@ -378,18 +378,18 @@ async def mail(ctx,*args):
     lt = []
     if 'A' in args or 'a' in args:
         lt.extend([                         #Email ID of Category A Students
-        'email1@gmail.com',
-        'email2@gmail.com',
+            'email1@gmail.com',
+            'email2@gmail.com',
         ])
     if 'B' in args or 'b' in args:
         lt.extend([                         #Email ID of Category B Students
-        'email3@gmail.com',
-        'email4@gmail.com',
+            'email3@gmail.com',
+            'email4@gmail.com',
         ])
     if 'Team' in args or 'team' in args or 'Team' in args:
         lt.extend([                         #Email ID of LIMIT Team Members
-        'arghyadip.chak16@gmail.com',
-        'email5@gmail.com',
+            'arghyadip.chak16@gmail.com',
+            'email5@gmail.com',
         ])send('\n'.join(lt))
 @client.event
 async def on_message(message):
